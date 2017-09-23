ea536647131f2574398b5b2e6d84c0100db33363
DESKTOP-1AG7R2H\Corvo <bigboyfc11@gmail.com>
2017-09-23 02:13:41 +0100

    BT27 Using DeprojectScreenToWorld

--- BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
@@ -53,7 +53,22 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) cons
 	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
 
 	// "De-project" the screen position of the crosshair to a world direction
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *LookDirection.ToString());
+	}
 	// Linetrace along that direction and see what we hit (up to max range)
 	return true;
 }
 
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldLocation; // To be discarded
+	return DeprojectScreenPositionToWorld(
+		ScreenLocation.X, 
+		ScreenLocation.Y, 
+		CameraWorldLocation, 
+		LookDirection
+	);
+}
